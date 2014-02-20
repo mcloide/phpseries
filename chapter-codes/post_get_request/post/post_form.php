515b132cf110d462247bef02b60f447f44d53398
Cristiano Diniz da Silva <cristiano.dasilva@zumba.com>
2014-02-20 13:20:07 -0500

    removing weird chars

--- chapter-codes/post_get_request/post/post_form.php
+++ chapter-codes/post_get_request/post/post_form.php
@@ -1,7 +1,7 @@
 <html>
 <body>
-	<form method='post' action=post_process.php'>
-		<input type=ÓtextÓ name='username' value='' /><br />
+	<form method='post' action='post_process.php'>
+		<input type='text' name='username' value='' /><br />
 		<input type='submit' value='submit form' />
 	</form>
 </body>
