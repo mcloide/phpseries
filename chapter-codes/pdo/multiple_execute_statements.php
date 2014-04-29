119aa9ae4ec95cb680f6b4d2d02e436eee8c014f
Cristiano Diniz da Silva <cristiano.dasilva@zumba.com>
2014-04-29 13:22:49 -0400

    center all things

--- chapter-codes/pdo/multiple_execute_statements.php
+++ chapter-codes/pdo/multiple_execute_statements.php
@@ -9,3 +9,23 @@ foreach ($types as $type) {
    $result = $sth->fetchAll();
    var_dump($result);
 }
+
+/** INSERTS **/
+
+$sth = $dbh->prepare("INSERT INTO fruits VALUES (:name, :color, :type)");
+ 
+$fruits = array(
+	array('name' => 'lemon', 'color' => 'green', 'type' => 'citrus'),
+	array('name' => 'orange', 'color' => 'orange', 'type' => 'citrus'),
+	array('name' => 'pinaple', 'color' => 'yellow', 'type' => '????'), // the type you eat, I guess
+);
+foreach ($fruits as $fruit) {
+   $sth->execute($fruit); 
+}
+
+$sth = $dbh->prepare('SELECT * FROM fruits WHERE type IN (:type)');
+$sth->execute(array('type' => 'citrus,????'));
+
+echo 'Printing inserted fruits';
+$result = $sth->fetchAll();
+var_dump($result);
